58f0b88ba805ab34a059c30578000b59357579cc
onnymm <phr2807@gmail.com>
2025-06-30 17:37:40 -0700

    Cambios menores en clase base de módulo de validaciones

--- lylac/_core/_submodules/_base_validations.py
+++ lylac/_core/_submodules/_base_validations.py
@@ -1,7 +1,9 @@
 from ._base_base_lylac import BaseBaseLylac
+from ._base_structure import BaseStructure
 
 class BaseValidations():
     _main: BaseBaseLylac
+    _strc: BaseStructure
 
     def initialize_model_validations(
         self,
