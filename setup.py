8b0dd9276b11b888fd4beb93d8e561ac57d07a46
onnymm <phr2807@gmail.com>
2025-11-14 09:20:38 -0700

    Actualizada versión

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     # Library name and version
     name="lylac",
-    version="2.3.9.2",
+    version="2.3.9.3",
 
     # Dependencies
     install_requires=[
